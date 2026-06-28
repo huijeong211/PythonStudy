83c2a1e976db5bb5ef9a378cbce45625953d021c
류희정 <wisebestgirl@gmail.com>
2026-06-28 21:32:55 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.01 ms, Memory: 11.3 MB -BaekjoonHub

---
+++ Python3/프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
@@ -0,0 +1,13 @@
+def solution(a, b):
+    
+    result1 = int(str(a)+ str(b))
+    
+    result2 = int(str(b) + str(a))
+    
+    if result1 >= result2:
+        return result1
+        
+    else:
+        return result2
+    
+    return answer
