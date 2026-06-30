dd2de0f7da147e7a1b4b6b1f68973cf5bda82890
류희정 <wisebestgirl@gmail.com>
2026-07-01 00:46:04 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.01 ms, Memory: 11.3 MB -BaekjoonHub

---
+++ Python3/프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.py
@@ -0,0 +1,13 @@
+def solution(num_list):
+    odd_str = ""
+    even_str = ""
+    
+    for num in num_list:
+        if num % 2 != 0:
+            odd_str += str(num)
+        else:
+            even_str += str(num)
+                            
+    answer = int(odd_str) + int(even_str)
+    
+    return answer
