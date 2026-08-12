7515494edc881cd15b59516dcdc39f4aa1b7b6e4
류희정 <wisebestgirl@gmail.com>
2026-08-12 23:53:23 +0900

    [level 0] Title: 2의 영역, Time: 0.77 ms, Memory: 13.3 MB -BaekjoonHub

---
+++ Python/프로그래머스/0/181894. 2의 영역/2의 영역.py
@@ -0,0 +1,10 @@
+def solution(arr):
+    if 2 not in arr:
+        return [-1]
+    
+    start = arr.index(2)
+    end = len(arr) -1 -arr[::-1].index(2)
+    
+    
+    
+    return arr[start:end +1]
