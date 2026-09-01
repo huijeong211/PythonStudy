a724e72bff713ac157e5542eb3b5c78d6b6bf852
류희정 <wisebestgirl@gmail.com>
2026-09-01 09:25:43 +0900

    [level 0] Title: 문자열 묶기, Time: 4.97 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ Python/프로그래머스/0/181855. 문자열 묶기/문자열 묶기.py
@@ -0,0 +1,9 @@
+def solution(strArr):
+    counts = {}
+    
+    for s in strArr:
+        length = len(s)
+        
+        counts[length] = counts.get(length,0)+1
+    
+    return max(counts.values())
